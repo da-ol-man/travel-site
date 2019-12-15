ea8967e19b8ea41b1d538dce70d1a3344d821144
Wayne Hattingh <waynehattingh@mail.com>
2019-12-15 10:37:14 +0100

    port

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
-    port: 8080, 
+    port: 3000,
     host: '0.0.0.0'
   },
   mode: 'development',
@@ -28,7 +28,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader','css-loader?url=false',{loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
       }
     ]
   }
