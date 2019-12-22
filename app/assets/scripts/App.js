243c41734f32c57c2ef053ec968c5fed428058d8
Wayne Hattingh <waynehattingh@mail.com>
2019-12-22 18:03:06 +0100

    JS lesson 1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,20 @@ import '../styles/styles.css'
 if (module.hot) {
   module.hot.accept()
 }
+
+/* lesson example code below this line */
+
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.favoriteColor = favColor;
+  this.greet = function() {
+    console.log("Hello, my nanme is " + this.name + " and my favorite color is " + this.favoriteColor + "!");
+  }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
